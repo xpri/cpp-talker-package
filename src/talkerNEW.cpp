252c486226e1034dce01d527c81bdb34483bb1ba
James Aspilcueta <jaspil007@gmail.com>
2026-01-16 14:04:14 -0500

    feat: condense the code with a lambda function (cookie points)

--- src/talkerNEW.cpp
+++ src/talkerNEW.cpp
@@ -1,5 +1,5 @@
 #include <chrono>
-#include <functional>
+// #include <functional>        // No need for this since lambda function is used instead of std::bind
 #include <memory>
 #include <string>
 
@@ -20,27 +20,33 @@ class TalkerPublisher : public rclcpp::Node
             // 10 means how many items can be stored in the buffer
             publisher_ = this->create_publisher<sandbox_interfaces::msg::SensorData>("sensor_topic", 10);
             
+            // NO NEED FOR THIS IF USING LAMBDA
             // timer_= this->create_wall_timer(500ms, std::bind(&TalkerPublisher::timer_callback, this));
-            timer_ = this->create_wall_timer(500ms [](){});
+
+            timer_ = this->create_wall_timer
+            (
+                500ms,
+                [this]()
+                {
+                    auto message = sandbox_interfaces::msg::SensorData();
+                    
+                    message.sensor_id = this->count_
+                    message.status = "Operating Normally!";
+                    message.temperature = "72";      // Saying in fahrenheit
+                    message.active = true;
+
+                    RCLCPP_INFO(this->get_logger(), "Publishing ID: #%d | Temperature = %d", message.sensor_id, message.temperature);
+                                        
+                    this->count_++;
+
+                    this->publisher_->publish(message);
+                }
+            );        
         }
     private:
-        void timer_callback()
-        {
-            auto message = sandbox_interfaces::msg::SensorData();
-
-            message.sensor_id = count_
-            message.status = "Operating Normally!";
-            message.temperature = "72";      // Saying in fahrenheit
-            message.active = true;
-
-            RCLCPP_INFO(this->get_logger(), "%s", std::format("Publishing ID: %d | Temp: %.2f", message.sensor_id, message.temperature).c_str())
-            
-            publisher_->publish(message);
-        }
-        
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<sandbox_interfaces::msg::SensorData>::SharedPtr publisher_;
-    size_t count_;
+        rclcpp::Publisher<sandbox_interfaces::msg::SensorData>::SharedPtr publisher_;
+        rclcpp::TimerBase::SharedPtr timer_;
+        size_t count_;
 };
 
 int main(int argc, char * argv[])
